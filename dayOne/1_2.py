2c2010a3c96c7afd5a051d2b4aa5d390118dbf70
rocksnotch <williamyouse8@gmail.com>
2023-12-05 16:50:49 -0500

    Update 1_2.py

--- dayOne/1_2.py
+++ dayOne/1_2.py
@@ -2,7 +2,7 @@ import re
 
 numberWord = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 numberNum = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-inputDict = {}
+inputArray = []
 inputStrings = []
 answer = 0
 
@@ -11,3 +11,18 @@ for line in f:
     inputStrings.append(line.strip())
 f.close()
 
+for i in inputStrings:
+    for idx, j in enumerate(numberWord):
+        results = re.finditer(j, i)
+        for obj in results:
+            inputArray.append([numberNum[idx], obj.start()])
+            
+    x = re.finditer("\d", i)
+    for obj in x:
+        inputArray.append([int(i[obj.start()]), obj.start()])
+        
+    inputArray.sort(key=lambda x: x[1])
+    temp = str(inputArray[0][0]) + str(inputArray[-1][0])
+    answer += int(temp)
+    inputArray = []
+print(answer)
